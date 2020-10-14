1ac42b95e16f436eca217ba82f06ddf969cdce4d
leitto-mk <53115848+leitto-mk@users.noreply.github.com>
2020-10-14 10:48:38 +0800

    Update Store Procedure Note

--- STORE PROCEDURE - SubjectColumn.sql
+++ STORE PROCEDURE - SubjectColumn.sql
@@ -6,14 +6,14 @@ BEGIN
     SET @sql = ''; 
     SELECT GROUP_CONCAT(
         DISTINCT 
-            CONCAT( 'MAX(IF(SubjName = ''', SubjName, ''', MidRecap, 0)) AS ', REPLACE(SubjName, ' ','_') ) ) 
-        INTO @sql 
-        FROM tbl_09_det_grades 
-        WHERE SubjName NOT IN('None','-')
-        AND Room = @homeroom
-        AND Semester = @semester
-        AND schoolyear = @period
-        ORDER BY SubjName ASC; 
+            CONCAT('MAX(IF(SubjName = ''', SubjName, ''', MidRecap, 0)) AS ', REPLACE(SubjName, ' ','_')) 
+        ) INTO @sql 
+    FROM tbl_09_det_grades 
+    WHERE SubjName NOT IN('None','-')
+    AND Room = @homeroom
+    AND Semester = @semester
+    AND schoolyear = @period
+    ORDER BY SubjName ASC; 
        
     SET @sql = CONCAT('SELECT NIS, FullName, ', @sql ,' FROM tbl_09_det_grades WHERE Room = "',@homeroom,'" AND semester = "',@semester,'" AND schoolyear = "',@period,'" GROUP BY NIS ORDER BY FullName, SubjName ASC'); 
     
