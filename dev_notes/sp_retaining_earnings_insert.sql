e1d74e71a928f348533898bb39c730c8c15948f5
leitto-mk <53115848+leitto-mk@users.noreply.github.com>
2021-12-22 18:24:37 +0800

    Update Stored Procedure

--- dev_notes/sp_retaining_earnings_insert.sql
+++ dev_notes/sp_retaining_earnings_insert.sql
@@ -9,32 +9,34 @@ BEGIN
    AND Year = val_year
    AND Month = val_month;
 
+   -- GET ACCUMULATED ACCNO AMOUNT --
+   SELECT @revenue := IFNULL((
+         SELECT SUM(Amount) FROM tbl_fa_transaction
+         WHERE Branch = val_branch
+         AND YEAR(TransDate) = val_year AND MONTH(TransDate) = val_month
+         AND AccType IN ('R', 'R1')
+   ),0);
+
+   SELECT @expense := IFNULL((
+         SELECT SUM(Amount) FROM tbl_fa_transaction
+         WHERE Branch = val_branch
+         AND YEAR(TransDate) = val_year AND MONTH(TransDate) = val_month
+         AND AccType IN ('E', 'E1')
+   ),0);
+
+   SELECT @retaining := (@revenue - @expense);
+
    -- REVENUE-EXPENSE EACH OF SELECTED MONTH --
    INSERT INTO `tbl_fa_retaining_earning` (Branch, Year, Month, Retaining)
-   SELECT branch, year, month,
-   (
-     (
-        SELECT SUM(Amount) FROM tbl_fa_transaction
-        WHERE Branch = val_branch
-        AND YEAR(TransDate) = val_year AND MONTH(TransDate) = val_month
-        AND AccType IN ('R', 'R1')
-     ) 
-     -
-     (
-        SELECT SUM(Amount) FROM tbl_fa_transaction
-        WHERE Branch = val_branch
-        AND YEAR(TransDate) = val_year AND MONTH(TransDate) = val_month
-        AND AccType IN ('E', 'E1')
-     )
-   ) AS retaining;
+   SELECT val_branch, val_year, val_month, @retaining;
 
    -- SUM EACH OF SELECTED MONTH --
    UPDATE `tbl_fa_retaining_earning` AS parent
    SET RetainingSum = (
       SELECT SUM(Retaining) FROM tbl_fa_retaining_earning
-      WHERE CtrlNo <= parent.CtrlNo
-      AND Branch = val_branch
+      WHERE Branch = val_branch
       AND Year = val_year
+      AND Month <= parent.Month
    )
    WHERE Branch = val_branch
    AND Year = val_year;
--- dev_notes/sp_retaining_earnings_update.sql
+++ dev_notes/sp_retaining_earnings_update.sql
@@ -3,23 +3,26 @@ CREATE PROCEDURE `retaining_earnings_update` (IN `val_branch` VARCHAR(255), IN `
 BEGIN
    START TRANSACTION;
 
+   -- GET ACCUMULATED ACCNO AMOUNT --
+   SELECT @revenue := IFNULL((
+         SELECT SUM(Amount) FROM tbl_fa_transaction
+         WHERE Branch = val_branch
+         AND YEAR(TransDate) = val_year AND MONTH(TransDate) = val_month
+         AND AccType IN ('R', 'R1')
+   ),0);
+
+   SELECT @expense := IFNULL((
+         SELECT SUM(Amount) FROM tbl_fa_transaction
+         WHERE Branch = val_branch
+         AND YEAR(TransDate) = val_year AND MONTH(TransDate) = val_month
+         AND AccType IN ('E', 'E1')
+   ),0);
+
+   SELECT @retaining := (@revenue - @expense);
+
    -- REVENUE-EXPENSE EACH OF SELECTED MONTH --
    UPDATE `tbl_fa_retaining_earning`
-   SET Retaining = (
-     (
-        SELECT SUM(Amount) FROM tbl_fa_transaction
-        WHERE Branch = val_branch
-        AND YEAR(TransDate) = val_year AND MONTH(TransDate) = val_month
-        AND AccType IN ('R', 'R1')
-     ) 
-     -
-     (
-        SELECT SUM(Amount) FROM tbl_fa_transaction
-        WHERE Branch = val_branch
-        AND YEAR(TransDate) = val_year AND MONTH(TransDate) = val_month
-        AND AccType IN ('E', 'E1')
-     )
-   )
+   SET Retaining = @retaining
    WHERE Branch = val_branch
    AND Year = val_year
    AND Month = val_month;
@@ -28,9 +31,9 @@ BEGIN
    UPDATE `tbl_fa_retaining_earning` AS parent
    SET RetainingSum = (
       SELECT SUM(Retaining) FROM tbl_fa_retaining_earning
-      WHERE CtrlNo <= parent.CtrlNo
-      AND Branch = val_branch
+      WHERE Branch = val_branch
       AND Year = val_year
+      AND Month <= parent.Month
    )
    WHERE Branch = val_branch
    AND Year = val_year;
