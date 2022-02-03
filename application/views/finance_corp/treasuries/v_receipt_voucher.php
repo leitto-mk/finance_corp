5642270c0a1242989c6a7371d17074fca964ed7f
leitto-mk <53115848+leitto-mk@users.noreply.github.com>
2022-02-04 05:01:35 +0800

    [Treasury]: Fix Text Color

--- application/views/finance_corp/treasuries/v_receipt_voucher.php
+++ application/views/finance_corp/treasuries/v_receipt_voucher.php
@@ -69,7 +69,7 @@
                             <tbody>
                                 <?php for($i =0; $i < count($list); $i++) : ?>
                                     <?php $docno = $list[$i]['DocNo']; ?>
-                                    <tr class="font-white sbold">
+                                    <tr class="font-dark sbold">
                                         <td align="center"><?= $list[$i]['TransDate'] ?></td>
                                         <td align="center"><?= $list[$i]['DocNo'] ?></td>
                                         <td align="center"><?= $list[$i]['TransType'] ?></td>
