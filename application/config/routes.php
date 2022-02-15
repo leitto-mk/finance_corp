c40995c7e29c02833ad28600a3693c79811e151e
leitto-mk <53115848+leitto-mk@users.noreply.github.com>
2022-02-15 17:07:43 +0800

    Update Routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,5 +51,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 */
 $route['default_controller'] = 'FinanceCorp';
 $route['finance'] = 'FinanceCorp';
+$route['cashadvance'] = 'Cash_adv';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
