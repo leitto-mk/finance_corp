486260961462dda057b79e2a362958bdbb36f6dd
pranayansalindeho <pranayansalindeho@gmail.com>
2022-02-22 14:15:30 +0700

    Update Style Report

--- application/views/financecorp/cashadvance/v_add_ca_withdraw.php
+++ application/views/financecorp/cashadvance/v_add_ca_withdraw.php
@@ -1,6 +1,6 @@
 <?php $this->load->view('header_footer/header'); ?>
 <body class="page-container-bg-solid">
-    <div class="page-wrapper">
+    <div class="page-wrapper bg-white">
         <div class="page-wrapper-row">
             <div class="page-wrapper-top">
                 <!-- BEGIN HEADER MENU -->
@@ -8,7 +8,7 @@
                     <div class="container-fluid">
                         <div class="hor-menu">
                             <ul class="nav navbar-nav">
-                                <li aria-haspopup="true" class="menu-dropdown classic-menu-dropdown active uppercase">
+                                <li aria-haspopup="true" class="menu-dropdown classic-menu-dropdown active uppercase" style="margin-left: -30px">
                                     <h4 style="color:#ffffff">Cash Advance Withdraw</h4>
                                 </li>
                             </ul>
@@ -258,7 +258,6 @@
             </form>
         </div>
     </div>
-</div>
 <script type="text/javascript">
     window.onload = load_function;
 
