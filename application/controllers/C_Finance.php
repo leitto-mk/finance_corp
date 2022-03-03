9a90e39f58ed6e52688363876c463455f2f1399f
leitto-mk <53115848+leitto-mk@users.noreply.github.com>
2022-03-03 21:27:32 +0800

    [Master]: Reformat COA Controller

--- application/controllers/C_Finance.php
+++ application/controllers/C_Finance.php
@@ -12,7 +12,7 @@ class C_Finance extends CI_Controller
   public function index()
   {
     $result = $this->get_coa_data();
-    
+
     $table_data = [
       'coa' => $result ?? []
     ];
@@ -248,9 +248,10 @@ class C_Finance extends CI_Controller
     return $result;
   }
 
-  function reset_coa(){
-      $this->finance->reset_coa();
-      $this->session->set_flashdata('success_reset', '<div class="alert alert-success alert-dismissable"> <button type="button" data-dismiss="alert" aria-hidden="true" class="close">&times;</button> <center><strong>Success!</strong> COA has been reset</center> </div>');
-      redirect('C_Finance');  
+  function reset_coa()
+  {
+    $this->finance->reset_coa();
+    $this->session->set_flashdata('success_reset', '<div class="alert alert-success alert-dismissable"> <button type="button" data-dismiss="alert" aria-hidden="true" class="close">&times;</button> <center><strong>Success!</strong> COA has been reset</center> </div>');
+    redirect('C_Finance');
   }
 }
