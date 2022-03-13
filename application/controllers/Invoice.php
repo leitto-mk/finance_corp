0190dc0b150c8b2e81546707b60fede8ada3a353
leitto-mk <53115848+leitto-mk@users.noreply.github.com>
2022-03-14 02:27:46 +0800

    [AR]: Create Invoice Form

--- application/controllers/Invoice.php
+++ application/controllers/Invoice.php
@@ -1,56 +1,54 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
+date_default_timezone_set('Asia/Makassar');
+
 class Invoice extends CI_Controller
 {
-  public function __construct()
-  {
-    parent::__construct();
-    $this->load->model([
-      'Mdl_corp_invoice' => 'invoice'
-    ]);
-  }
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model([
+			'Mdl_corp_invoice' => 'invoice'
+		]);
+	}
 
-  public function index()
-  {
-    $title = 'Invoice Module';
-    $script = '<script src="' . base_url('js/invoice/dashboard/dashboard.js') . '" type="module"></script>';
+	public function index()
+	{
+		$title = 'Invoice Module';
+		$script = '<script src="' . base_url('js/invoice/dashboard/dashboard.js') . '" type="module"></script>';
 
-    $data_view = [
-      'title' => 'Dashboard'
-    ];
-    $content = $this->load->view('financecorp/invoice/content/dashboard', $data_view, true);
+		$data_view = [
+			'title' => 'Dashboard'
+		];
+		$content = $this->load->view('financecorp/invoice/content/dashboard', $data_view, true);
 
-    $data = [
-      'title' => $title,
-      'content' => $content,
-      'script' => $script,
-    ];
-    $this->load->view('financecorp/invoice/layout/main', $data);
-  }
+		$data = [
+			'title' => $title,
+			'content' => $content,
+			'script' => $script,
+		];
+		$this->load->view('financecorp/invoice/layout/main', $data);
+	}
 
-  public function view_create_invoice()
-  {
-    $title = 'Create Invoice';
-    $script = '';
+	public function create_invoice()
+	{
+		$content_data = [
+			'title' => 'Create Invoice',
+		];
 
-    $data_view = [
-      'title' => 'Create New Invoice'
-    ];
-    $content = $this->load->view('financecorp/invoice/content/create_invoice', $data_view, true);
+		$content = $this->load->view('financecorp/invoice/content/new_invoice', $content_data, TRUE);
 
-    $data = [
-      'title' => $title,
-      'content' => $content,
-      'script' => $script,
-      'no_sidebar' => true
-    ];
-    $this->load->view('financecorp/invoice/layout/main', $data);
-  }
+		$data = [
+			'form' => $content
+		];
 
-  public function get_approval()
-  {
-    $data = $this->invoice->M_get_invoice_approval();
+		$this->load->view('financecorp/invoice/layout/header_footer_form', $data);
+	}
 
-    echo json_encode($data);
-  }
+	public function get_approval()
+	{
+		$data = $this->invoice->M_get_invoice_approval();
+
+		echo json_encode($data);
+	}
 }
---
+++ application/views/financecorp/invoice/layout/header_footer_form.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+<!-- BEGIN HEAD -->
+
+<head>
+  <meta charset="utf-8" />
+  <title><?= $title ?></title>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta content="width=device-width, initial-scale=1" name="viewport" />
+  <!-- BEGIN GLOBAL MANDATORY STYLES -->
+  <!-- <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" /> -->
+  <link href="<?= base_url('assets') ?>/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+  <link href="<?= base_url('assets') ?>/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
+  <link href="<?= base_url('assets') ?>/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+  <link href="<?= base_url('assets') ?>/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
+  <!-- END GLOBAL MANDATORY STYLES -->
+  <!-- PAGE LEVEL -->
+
+  <!-- Datatable -->
+  <link href="<?= base_url('assets'); ?>/global/plugins/datatables/datatables.min.css" rel="stylesheet" type="text/css" />
+  <link href="<?= base_url('assets'); ?>/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.css" rel="stylesheet" type="text/css" />
+  <link href="<?= base_url('assets'); ?>/js/rowGroup.dataTables.min.css" rel="stylesheet" type="text/css" />
+
+  <!-- Picker -->
+  <link href="<?= base_url('assets') ?>/global/plugins/bootstrap-daterangepicker/daterangepicker.min.css" rel="stylesheet" type="text/css" />
+  <link href="<?= base_url('assets') ?>/global/plugins/bootstrap-datepicker/css/bootstrap-datepicker3.min.css" rel="stylesheet" type="text/css" />
+  <link href="<?= base_url('assets') ?>/global/plugins/bootstrap-timepicker/css/bootstrap-timepicker.min.css" rel="stylesheet" type="text/css" />
+  <link href="<?= base_url('assets') ?>/global/plugins/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css" rel="stylesheet" type="text/css" />
+
+  <!-- Select 2 -->
+  <link href="<?= base_url('assets') ?>/global/plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css" />
+  <link href="<?= base_url('assets') ?>/global/plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css" />
+
+  <!-- Sweet Alert -->
+  <link href="<?= base_url('assets') ?>/global/plugins/bootstrap-sweetalert/sweetalert.css" rel="stylesheet" type="text/css" />
+
+  <!-- END PAGE LEVEL -->
+  <!-- BEGIN THEME GLOBAL STYLES -->
+  <link href="<?= base_url('assets') ?>/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css" />
+  <link href="<?= base_url('assets') ?>/global/css/plugins.min.css" rel="stylesheet" type="text/css" />
+  <!-- END THEME GLOBAL STYLES -->
+
+  <link href="<?= base_url('assets') ?>/pages/css/invoice-2.min.css" rel="stylesheet" type="text/css" />
+
+  <!-- BEGIN THEME LAYOUT STYLES -->
+  <link href="<?= base_url('assets') ?>/layouts/layout2/css/layout.min.css" rel="stylesheet" type="text/css" />
+  <link href="<?= base_url('assets') ?>/layouts/layout2/css/themes/blue.min.css" rel="stylesheet" type="text/css" id="style_color" />
+  <link href="<?= base_url('assets') ?>/layouts/layout2/css/custom.min.css" rel="stylesheet" type="text/css" />
+  <!-- END THEME LAYOUT STYLES -->
+  <link rel="shortcut icon" href="favicon.ico" />
+</head>
+<!-- END HEAD -->
+
+<body class="page-header-fixed page-sidebar-closed-hide-logo page-container-bg-solid">
+  <!-- BEGIN CONTAINER -->
+  <div class="page-container" style="margin-top: 0">
+    <!-- BEGIN CONTENT -->
+    <div class="page-content-wrapper">
+      <!-- BEGIN CONTENT BODY -->
+      <div class="page-content" style="margin-left: 0;">
+        <?= $form ?>
+      </div>
+      <!-- END CONTENT BODY -->
+    </div>
+    <!-- END CONTENT -->
+  </div>
+  <!-- END CONTAINER -->
+  <!-- BEGIN FOOTER -->
+  <div class="page-footer">
+    <div class="page-footer-inner">
+      <a href="#" target="_blank" class="font-white"><?= date('Y') ?> &copy; ABase</a> &nbsp;|&nbsp;
+      <div class="scroll-to-top">
+        <i class="icon-arrow-up"></i>
+      </div>
+    </div>
+  </div>
+  <!-- END FOOTER -->
+  <!--[if lt IE 9]>
+<script src="<?= base_url('assets') ?>/global/plugins/respond.min.js"></script>
+<script src="<?= base_url('assets') ?>/global/plugins/excanvas.min.js"></script> 
+<script src="<?= base_url('assets') ?>/global/plugins/ie8.fix.min.js"></script> 
+<![endif]-->
+  <!-- BEGIN CORE PLUGINS -->
+  <script src="<?= base_url('assets') ?>/global/plugins/jquery.min.js" type="text/javascript"></script>
+  <script src="<?= base_url('assets') ?>/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+  <script src="<?= base_url('assets') ?>/global/plugins/js.cookie.min.js" type="text/javascript"></script>
+  <script src="<?= base_url('assets') ?>/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+  <script src="<?= base_url('assets') ?>/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
+  <script src="<?= base_url('assets') ?>/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"></script>
+  <!-- END CORE PLUGINS -->
+  <!-- BEGIN THEME GLOBAL SCRIPTS -->
+  <script src="<?= base_url('assets') ?>/global/scripts/app.min.js" type="text/javascript"></script>
+  <!-- END THEME GLOBAL SCRIPTS -->
+  <!-- BEGIN THEME LAYOUT SCRIPTS -->
+
+  <script src="<?= base_url('assets') ?>layouts/layout2/scripts/layout.min.js" type="text/javascript"></script>
+  <script src="<?= base_url('assets') ?>layouts/layout2/scripts/demo.min.js" type="text/javascript"></script>
+  <script src="<?= base_url('assets') ?>layouts/global/scripts/quick-sidebar.min.js" type="text/javascript"></script>
+  <script src="<?= base_url('assets') ?>layouts/global/scripts/quick-nav.min.js" type="text/javascript"></script>
+
+  <!-- Datatable -->
+  <script src="<?= base_url('assets'); ?>/global/plugins/datatables/datatables.min.js" type="text/javascript"></script>
+  <script src="<?= base_url('assets'); ?>/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.js" type="text/javascript"></script>
+  <!-- Datatable -->
+
+  <script src="<?= base_url('assets') ?>/global/plugins/moment.min.js" type="text/javascript"></script>
+
+  <!-- Picker -->
+  <script src="<?= base_url('assets') ?>/global/plugins/bootstrap-daterangepicker/daterangepicker.min.js" type="text/javascript"></script>
+  <script src="<?= base_url('assets') ?>/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js" type="text/javascript"></script>
+  <script src="<?= base_url('assets') ?>/global/plugins/bootstrap-timepicker/js/bootstrap-timepicker.min.js" type="text/javascript"></script>
+  <script src="<?= base_url('assets') ?>/global/plugins/bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js" type="text/javascript"></script>
+  <script src="<?= base_url('assets') ?>/global/plugins/counterup/jquery.waypoints.min.js" type="text/javascript"></script>
+  <script src="<?= base_url('assets') ?>/global/plugins/counterup/jquery.counterup.min.js" type="text/javascript"></script>
+  <!-- Picker -->
+
+  <!-- Select 2 -->
+  <script src="<?= base_url('assets') ?>/global/plugins/select2/js/select2.full.min.js" type="text/javascript"></script>
+  <!-- Select 2 -->
+
+  <!-- Sweet Alert -->
+  <script src="<?= base_url('assets') ?>/global/plugins/bootstrap-sweetalert/sweetalert.min.js" type="text/javascript"></script>
+  <!-- Sweet Alert -->
+
+  <!-- END THEME LAYOUT SCRIPTS -->
+</body>
+
+</html>
